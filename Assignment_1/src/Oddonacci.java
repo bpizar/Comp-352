a9cc8e6e151131a0ae9bd51cdf187056c2f250cf
bpizar <benjaminpizarroribera@gmail.com>
2022-01-26 11:39:38 -0500

    V2 added recursiveOddo function

--- Assignment_1/src/Oddonacci.java
+++ Assignment_1/src/Oddonacci.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class Oddonacci 
 {
-
+	//counter to track number recursion calls
+	private static int numOfCalls = 0;
+	
 	public static void main(String[] args)
 	{
 		Scanner kb =  new Scanner(System.in);
@@ -10,15 +12,57 @@ public class Oddonacci
 		System.out.print("Input Oddo(__): ");
 		int n = kb.nextInt();
 		System.out.println();
-	
-		long odd = oddo(n);
+		
+		//test recursive oddonacci
+		long[] baseArr = {0,0,0};
+		long [] index = {1,1,1};
+		long odd = recursiveOddo(n ,baseArr,index);
 		System.out.println();
-		System.out.println("Oddo(" + n + ") =  " + odd);
+		System.out.println("recursiveOddo(" + n + ") =  " + odd);
+		System.out.println("Number of Calls: " + numOfCalls);
+		System.out.println();
+		System.out.println();
+
+		//test linear oddonacci
+		long odd2 = oddo(n);
+		System.out.println();
+		System.out.println("Oddo(" + n + ") =  " + odd2);
 
 		kb.close();
 		
 	}
-	
+	public static long recursiveOddo (int n,long[] baseArr,long [] index)
+	{	
+		//initialize index values
+		if (numOfCalls == 0)
+		{
+			index[0] = 1;
+			index[1] = 1;
+			index[2] = 1;
+			System.out.print( index[0] + " " + index[1] + " " + index[2] + " ");
+		}
+		//base case	
+		if ( n <= 3)
+			return baseArr[2];
+		else
+		{
+			baseArr[0] = index[1];
+			baseArr[1] = index[2];
+			baseArr[2] = index[0] + index[1] + index[2];
+			
+			System.out.print( baseArr[2] + " ");
+			
+			index[0] = baseArr[0];
+			index[1] = baseArr[1];
+			index[2] = baseArr[2];
+
+		}
+		
+		//recursive step
+		numOfCalls++;
+		return recursiveOddo(n-1,index,baseArr);
+				
+	}
 	/**
 	 * Oddonacci calculates the Oddonacci sequence up to position n
 	 * and the value at position n
