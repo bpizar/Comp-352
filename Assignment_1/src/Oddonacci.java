1ad0bb9c6c90094290d73087dab763495b86ac5d
bpizar <benjaminpizarroribera@gmail.com>
2022-01-27 17:19:47 -0500

    V3 modified recursiveOddo to not use arrays

--- Assignment_1/src/Oddonacci.java
+++ Assignment_1/src/Oddonacci.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Oddonacci 
 {
-	//counter to track number recursion calls
+	//counter to track number recursion calls in recursiveOddo
 	private static int numOfCalls = 0;
-	
+	//counter to track number of iterations in linearOddo
+	private static int numOfIterations = 0;
+
 	public static void main(String[] args)
 	{
 		Scanner kb =  new Scanner(System.in);
@@ -14,76 +16,69 @@ public class Oddonacci
 		System.out.println();
 		
 		//test recursive oddonacci
-		long[] baseArr = {0,0,0};
-		long [] index = {1,1,1};
-		long odd = recursiveOddo(n ,baseArr,index);
+		long odd = recursiveOddo(n ,1,1,1);
 		System.out.println();
 		System.out.println("recursiveOddo(" + n + ") =  " + odd);
 		System.out.println("Number of Calls: " + numOfCalls);
 		System.out.println();
-		System.out.println();
 
 		//test linear oddonacci
-		long odd2 = oddo(n);
+		long odd2 = linearOddo(n);
 		System.out.println();
 		System.out.println("Oddo(" + n + ") =  " + odd2);
+		System.out.println("Number of Iterations: " + numOfIterations);
+
 
 		kb.close();
 		
 	}
-	public static long recursiveOddo (int n,long[] baseArr,long [] index)
+	/**
+	 * recursiveOddo calculates the oddonacci value at position n
+	 * It is O(n)
+	 * @param n
+	 * @param i
+	 * @param j
+	 * @param k
+	 * @return i+j+k for (n > 3) OR k for (n <= 3)
+	 */
+	public static long recursiveOddo (int n,long i , long j, long k)
 	{	
-		//initialize index values
-		if (numOfCalls == 0)
-		{
-			index[0] = 1;
-			index[1] = 1;
-			index[2] = 1;
-			System.out.print( index[0] + " " + index[1] + " " + index[2] + " ");
-		}
 		//base case	
 		if ( n <= 3)
-			return baseArr[2];
+		{
+			k = 1;
+			return k;
+		}
+		//recursive step
 		else
 		{
-			baseArr[0] = index[1];
-			baseArr[1] = index[2];
-			baseArr[2] = index[0] + index[1] + index[2];
-			
-			System.out.print( baseArr[2] + " ");
-			
-			index[0] = baseArr[0];
-			index[1] = baseArr[1];
-			index[2] = baseArr[2];
-
+			numOfCalls++; // count number of recursive calls
+			k = recursiveOddo(n - 1,j,k,i+j+k);
 		}
-		
-		//recursive step
-		numOfCalls++;
-		return recursiveOddo(n-1,index,baseArr);
-				
+		return i+j+k;						
 	}
+	
 	/**
-	 * Oddonacci calculates the Oddonacci sequence up to position n
+	 * linearOddo calculates the oddonacci sequence up to position n
 	 * and the value at position n
-	 * It is O(n) running in one for
+	 * It is O(n) running in one for loop
 	 * @param n
 	 * @return baseArr[2]
 	 */
-	public static long oddo(int n)
+	public static long linearOddo(int n)
 	{
 		
 		long i = 1, j = 1, k = 1;
 		long[] baseArr = {i,j,k};
-		System.out.print(baseArr[0] + " " + baseArr[1] + " " + baseArr[2] + " ");
+		//System.out.print(baseArr[0] + " " + baseArr[1] + " " + baseArr[2] + " ");
 		
-		for (int c = 0 ; c < n - 3 ; c++)
+		for (int c = 0 ; c < n - 3 ; c++,numOfIterations++) // count number of iterations
 		{
 			baseArr[0] = j;
 			baseArr[1] = k;
 			baseArr[2] = i+j+k;
 			
-			System.out.print( baseArr[2] + " ");
+			//System.out.print( baseArr[2] + " ");
 			
 			i = baseArr[0];
 			j = baseArr[1];
